08b229c91f0a52044a511c3bc041631174c0c89c
david <david@venturplex.com>
2021-03-10 18:58:11 -0700

    Create api and application with NX

---
+++ apps/level-up-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'level-up-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/level-up-api',
+};
